da68439edf55645574644fa5632364b04ad1747c
TASK FORCE 141 <ozhan-yildirim@hotmail.com>
2022-02-25 05:50:53 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace followers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Fugaz One", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(308, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "instagram tool";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(350, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(350, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 34);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Fugaz One", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 49);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Fugaz One", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-6, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(283, 49);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(540, 201);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Fugaz One", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-6, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 49);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Account";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(352, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 34);
+            this.textBox3.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(5, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(233, 204);
+            this.listBox1.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(7, 238);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 220);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TAKİPÇİ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(583, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 102);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "VERİ ÇEK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.ForeColor = System.Drawing.Color.White;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(6, 16);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(232, 204);
+            this.listBox2.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Location = new System.Drawing.Point(271, 238);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(244, 220);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "TAKİP ETMEYENLER";
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox3.ForeColor = System.Drawing.Color.White;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(6, 16);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(232, 204);
+            this.listBox3.TabIndex = 6;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox2);
+            this.groupBox4.Location = new System.Drawing.Point(535, 238);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(244, 220);
+            this.groupBox4.TabIndex = 12;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "TAKİP EDİLENLER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 470);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace followers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            //renk fln veriyorum burası boş 
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml("#7900FF");
+            label1.ForeColor = System.Drawing.ColorTranslator.FromHtml("#93FFD8");
+            groupBox1.ForeColor = System.Drawing.ColorTranslator.FromHtml("#548CFF");
+            listBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#548CFF");
+            listBox2.BackColor = System.Drawing.ColorTranslator.FromHtml("#548CFF");
+            listBox3.BackColor = System.Drawing.ColorTranslator.FromHtml("#548CFF");
+            button1.BackColor = System.Drawing.ColorTranslator.FromHtml("#548CFF");
+
+
+
+
+
+        }
+
+     
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();  //web driver açıyorum
+            driver.Navigate().GoToUrl("https://www.instagram.com"); // web driverin ha bu verdiğim adrese gitmesini sağlıyorum
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("Siteye Gidildi!");
+            Thread.Sleep(2000); //bunu aralara serpiştirmek önemli hızlı hızlı yapınca genelde hata veriyor
+
+            //biraz html css felan 
+            IWebElement userName = driver.FindElement(By.Name("username"));
+            IWebElement password = driver.FindElement(By.Name("password"));
+            IWebElement loginBtn = driver.FindElement(By.CssSelector(".sqdOP.L3NKy.y3zKF"));  //bunu almayı gösterdim konuda
+
+
+            
+            //yukardki yerlere gidecek şeyleri ayarlıyorum
+            string kulad, sif, gidhesap;
+            kulad = textBox1.Text;
+            sif = textBox2.Text;
+            gidhesap = textBox3.Text;
+
+            Thread.Sleep(1500);
+            userName.SendKeys(kulad);
+            Thread.Sleep(1500);
+            password.SendKeys(sif);
+            Thread.Sleep(1500);
+            loginBtn.Click();
+            Thread.Sleep(1500);
+            Console.WriteLine("Hesap Bilgileri Girildi!");
+
+            Thread.Sleep(1500);
+            driver.Navigate().GoToUrl($"https://www.instagram.com/" + gidhesap + "/");
+            Console.WriteLine("Profile Yönlendirildi!");
+
+
+            Thread.Sleep(1500);
+
+
+
+            Thread.Sleep(1500);
+            
+
+                Thread.Sleep(1500);
+            // burası bazen hata veriyor sebebini bilmiyorum çünkü css bilmiyorum
+                IWebElement followerLink = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a > div"));
+                followerLink.Click();
+                Thread.Sleep(2000);
+
+                //ScrollDown Start
+                //isgrP
+
+            //biraz js 
+                string jsCommand = "" +
+                    "sayfa = document.querySelector('.isgrP');" +     //takipçiler sekmesindeki scrollu hareket ettirmek için 
+                    "sayfa.scrollTo(0,sayfa.scrollHeight);" +
+                    "var sayfaSonu = sayfa.scrollHeight;" +
+                    "return sayfaSonu;";
+
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                var sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+                while (true)
+                {
+                    var son = sayfaSonu;
+                    Thread.Sleep(1500);
+                    sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                    if (son == sayfaSonu)
+                        break;
+                }
+
+                //ScrollDown End
+
+
+                //Takipçi Listeleme Start
+
+                int sayac = 0;
+                IReadOnlyCollection<IWebElement> follwers = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+
+                foreach (IWebElement follower in follwers)
+                {
+
+                    sayac++;
+
+
+                    listBox1.Items.Add(" ==> " + follower.Text);
+
+                }
+                groupBox2.Text = ("takipçi:" + sayac + "kişi vardır");
+
+
+                //Takipçi Listeleme END
+
+                driver.Navigate().GoToUrl($"https://www.instagram.com/" + gidhesap + "/");
+                Console.WriteLine("Profile Yönlendirildi!");
+                Thread.Sleep(2000);
+                IWebElement followsLink = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a"));
+                followsLink.Click();
+                Thread.Sleep(1000);
+
+
+                //ScrollDown Start
+                //isgrP
+                string jsCommand1 = "" +
+                    "sayfa = document.querySelector('.isgrP');" +
+                    "sayfa.scrollTo(0,sayfa.scrollHeight);" +
+                    "var sayfaSonu = sayfa.scrollHeight;" +
+                    "return sayfaSonu;";
+
+                IJavaScriptExecutor js1 = (IJavaScriptExecutor)driver;
+                var sayfaSonu1 = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+                while (true)
+                {
+                    var son = sayfaSonu;
+                    Thread.Sleep(1000);
+                    sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                    if (son == sayfaSonu)
+                        break;
+                }
+
+                //ScrollDown End
+
+
+                //Takip edilen Listeleme Start
+
+                int sayac1 = 0;
+                IReadOnlyCollection<IWebElement> follows = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+
+                foreach (IWebElement follower in follows)
+                {
+
+                    sayac1++;
+
+                    listBox2.Items.Add(" ==> " + follower.Text);
+
+                }
+                groupBox4.Text = ("takip edilen:" + sayac1 + "kişi vardır");
+
+
+                //Takip edilen Listeleme END
+
+                string[] veriler1 = new string[listBox1.Items.Count];
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    veriler1[i] = listBox1.Items[i].ToString();
+                }
+
+                List<string> veriler2 = new List<string>();
+                foreach (var item in listBox1.Items)
+                {
+                    veriler2.Add(item.ToString());
+                }
+
+                var veriler3 = listBox1.Items.Cast<String>().ToArray();
+
+
+
+
+
+
+                string[] veriler4 = new string[listBox2.Items.Count];
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                {
+                    veriler4[i] = listBox2.Items[i].ToString();
+                }
+
+                List<string> veriler5 = new List<string>();
+                foreach (var item in listBox1.Items)
+                {
+                    veriler5.Add(item.ToString());
+                }
+
+                var veriler6 = listBox2.Items.Cast<String>().ToArray();
+
+                listBox3.DataSource = veriler4.Except(veriler3).ToList();
+
+
+
+
+                driver.Close();
+                Console.WriteLine("----------------------");
+
+
+
+            
+        }
+
+       
+
+      
+ 
+
+    }
+}
+
+       
+    
+
+
